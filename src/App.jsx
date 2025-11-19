49fe08fb33c7f4d40524b58fcd6dc46464a3c6ac
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-11-19 07:33:18 +0330

    Personal Hook-part1

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const App = () => {
+
+  const [page , setPage] = useState("users");
+
+  return(
+    <div className="min-h-screen bg-linear-to-br from-blue-400 to-amber-500 p-6">
+      <div className="max-w-4xl mx-auto bg-white/30 rounded shadow-md overflow-hidden">
+        <div className="p-6">
+          <h1 className="text-3xl font-bold text-center text-emerald-500 mb-6">React App</h1>
+
+          <div className="flex justify-center mb-8 gap-4">
+            <button
+            onClick={() => setPage("users")}
+            className={`px-6 py-3 rounded font-medium transition-all duration-200
+              ${page === "users" 
+                ? "bg-amber-500 text-white shadow-md"
+                : "bg-gray-500 text-gray-700 hover:bg-gray-200"
+              }`}
+            >
+              کاربران
+            </button>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default App;
