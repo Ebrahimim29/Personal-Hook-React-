1514a1caabdb85ddb44244a8230b0f5549edaee1
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-11-22 07:35:16 +0330

    React Router-part1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Users from "./Users";
 import Posts from "./Posts";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 const App = () => {
 
-  const [page , setPage] = useState("users");
+  // const [page , setPage] = useState("users");
 
   return(
     <div className="min-h-screen bg-linear-to-br from-blue-400 to-amber-500 p-6">
@@ -14,30 +15,37 @@ const App = () => {
 
           <div className="flex justify-center mb-8 gap-4">
             <button
-            onClick={() => setPage("users")}
-            className={`px-6 py-3 rounded font-medium transition-all duration-200
-              ${page === "users" 
-                ? "bg-amber-500 text-white shadow-md"
-                : "bg-gray-500 text-gray-700 hover:bg-gray-200"
-              }`}
+            // onClick={() => setPage("users")}
+            className={`px-6 py-3 rounded font-medium transition-all duration-200`}
+              // ${page === "users" 
+              //   ? "bg-amber-500 text-white shadow-md"
+              //   : "bg-gray-500 text-gray-700 hover:bg-gray-200"
+              // }`}
             >
               کاربران
             </button>
             <button
-            onClick={() => setPage("posts")}
-            className={`px-6 py-3 rounded font-medium transition-all duration-200
-              ${page === "posts"
-                ? "bg-emerald-700 text-white shadow-md"
-                : "bg-gray-500 text-gray-700 hover:bg-gray-200"
-              }`}
+            // onClick={() => setPage("posts")}
+            className={`px-6 py-3 rounded font-medium transition-all duration-200`}
+              // ${page === "posts"
+              //   ? "bg-emerald-700 text-white shadow-md"
+              //   : "bg-gray-500 text-gray-700 hover:bg-gray-200"
+              // }`}
             >
               پست ها
             </button>
           </div>
 
           <div className="bg-gray-200 rounded p-4">
-            {page === "users" && <Users/>}
-            {page === "posts" && <Posts/>}
+            {/* {page === "users" && <Users/>}
+            {page === "posts" && <Posts/>} */}
+
+            <BrowserRouter>
+              <Routes>
+                <Route path="/users" element={<Users/>}></Route>
+                <Route path="/posts" element={<Posts/>}></Route>
+              </Routes>
+            </BrowserRouter>
           </div>
         </div>
       </div>
